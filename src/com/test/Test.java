c25859d3dff1b7d98e569169eab7f40b490a3bd2
EMairot <EMairot@Emmanuel-PC.QUALIMS.LOCAL>
2017-03-01 12:24:38 +0100

    eee

---
+++ src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+	private String test;
+	
+	public void setMessage(String message){
+	      this.test  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Esse : " + test);
+	   }
+
+}
